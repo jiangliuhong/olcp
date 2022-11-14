de69c8e9c807a9ae186b101c55ef7fc76a0c621a
jiangliuhong <ja_rome@163.com>
2022-11-14 15:39:14 +0800

    rename engine to context

--- olcp-data/olcp-data-context/src/main/java/top/jiangliuhong/olcp/data/DataContextAutoConfigure.java
+++ olcp-data/olcp-data-context/src/main/java/top/jiangliuhong/olcp/data/DataContextAutoConfigure.java
@@ -1,10 +1,9 @@
 package top.jiangliuhong.olcp.data;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 //@ComponentScan("top.jiangliuhong.olcp.data.sql")
-public class DataEngineAutoConfigure {
+public class DataContextAutoConfigure {
 
 }
