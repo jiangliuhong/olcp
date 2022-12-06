bd8976189365615d50061e2bfaf68ea24b552d25
jarome <jaromejiang@yunify.com>
2022-12-06 23:24:06 +0800

    修改token默认过期时间

--- olcp-common/olcp-common-auth/src/main/java/top/jiangliuhong/olcp/auth/properties/JwtProperties.java
+++ olcp-common/olcp-common-auth/src/main/java/top/jiangliuhong/olcp/auth/properties/JwtProperties.java
@@ -11,6 +11,6 @@ public class JwtProperties {
     private String tokenHeader = "Authorization";
     private String tokenHead = "Bearer ";
     private String secret = "olcp-secret";
-    // 60*60*24 = 604800 second
-    private Integer expiration = 604800;
+    // 默认2小时，7200秒
+    private Integer expiration = 7200;
 }
